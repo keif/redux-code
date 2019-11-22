65aaad2531dc1bcaaac52c10cffcdee043372219
Keith T. Baker <hello@keif.name>
2019-11-22 02:50:31 -0500

    delete properties with omit

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,6 +1,10 @@
 import { EDIT_STREAM, FETCH_STREAM, DELETE_STREAM, CREATE_STREAM } from '../actions/types';
+import _ from 'lodash'
+
 export default (state = {}, action) => {
   switch (action.type) {
+    case DELETE_STREAM:
+        return _.omit(action.payload, state)
     case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload }
     case EDIT_STREAM:
