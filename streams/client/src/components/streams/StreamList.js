0be0b1aef9b06ee935254bc53cf03ab9f5dfe78d
Keith T. Baker <hello@keif.name>
2019-11-22 03:10:44 -0500

    rendering all streams

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -3,15 +3,39 @@ import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
 const StreamList = (props) => {
-  const { fetchStreams } = props
+  const { fetchStreams, streams } = props
+
+  const renderList = () => {
+    return streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">
+              {stream.description}
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
 
   useEffect(() => {
     fetchStreams()
   }, [])
 
   return (
-    <div>StreamList</div>
+    <>
+      <h2>Streams</h2>
+      <div className="ui celled list">
+        {renderList()}
+      </div>
+    </>
   )
 }
 
-export default connect(null, { fetchStreams })(StreamList)
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) }
+}
+export default connect(mapStateToProps, { fetchStreams })(StreamList)
