ebd4d55ce1b32bc34eab107ee83d1423fe7b5b2b
Keith T. Baker <hello@keif.name>
2019-11-23 16:22:49 -0500

    conditionally show edit/delete

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,14 +1,31 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Stream } from 'stream';
 
 const StreamList = (props) => {
-  const { fetchStreams, streams } = props
+  const { fetchStreams, streams, currentUserId } = props
+
+  const renderAdmin = (stream) => {
+    if (stream.userId === currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">
+            Edit
+          </button>
+          <button className="ui button negative">
+            Delete
+          </button>
+        </div>
+      )
+    }
+  }
 
   const renderList = () => {
     return streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -36,6 +53,11 @@ const StreamList = (props) => {
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) }
+  const { auth, streams } = state
+
+  return {
+    streams: Object.values(streams),
+    currentUserId: auth.userId
+  }
 }
 export default connect(mapStateToProps, { fetchStreams })(StreamList)
