197379783e2fa6878ba1d6254e532b5c1c2dfb9a
Keith T. Baker <hello@keif.name>
2019-11-24 15:05:11 -0500

    selecting a language

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = (props) => {
+  const [language, setLanguage] = useState('english')
+
+  const onLanguageChange = (language) => {
+    setLanguage(language)
+  }
+
   return (
-    <div className="ui container">App</div>
+    <div className="ui container">
+      <div>
+        Select a language:
+        <i className="flag us" onClick={() => onLanguageChange('english')}/>
+        <i className="flag nl" onClick={() => onLanguageChange('dutch')}/>
+      </div>
+      {language}
+    </div>
   )
 }
 
