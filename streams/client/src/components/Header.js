86e6ec803171025f4ad8994ef803f82e057efb85
Keith T. Baker <hello@keif.name>
2019-11-24 02:08:09 -0500

    viewing a stream

--- streams/client/src/components/Header.js
+++ streams/client/src/components/Header.js
@@ -5,7 +5,7 @@ import GoogleAuth from './GoogleAuth';
 const Header = (props) => {
   return (
     <div className="ui secondary pointing menu">
-      <Link className="item" to="/">Streamer</Link>
+      <Link className="item" to="/">Streamy</Link>
 
       <div className="right menu">
         <Link className="item" to="/">All Streams</Link>
--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -38,7 +38,7 @@ const StreamList = (props) => {
           {renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
             <div className="description">
               {stream.description}
             </div>
