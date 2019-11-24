40213987152381ca4a38fe78d3f42553102b2d7f
Keith T. Baker <hello@keif.name>
2019-11-24 02:35:12 -0500

    showing a stream

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { fetchStream } from '../../actions/index';
 
 const StreamShow = (props) => {
+  const { fetchStream, match, stream } = props
+
+  useEffect(() => {
+    fetchStream(match.params.id)
+  }, [match])
+
+  if (!stream) {
+    return (
+      <>
+        Loading...
+      </>
+    )
+  }
+
   return (
-    <div>StreamShow</div>
+    <>
+      <h1>{stream.title}</h1>
+      <h5>{stream.description}</h5>
+    </>
   )
 }
 
-export default StreamShow
+const mapStateToProps = (state, ownProps) => {
+  const { streams } = state
+  const { match } = ownProps
+
+  return {
+    stream: streams[match.params.id]
+  }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow)
